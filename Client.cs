c663c1f67e89ec48ff6effc54f7aeecffb820a4a
Hamza <zrewlate-hamza@outlook.fr>
2023-04-16 02:43:43 -0400

    Ajouter la classe Client à la classe principale

---
+++ Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQL_Evaluation_Sommative_3
+{
+    internal class Client
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Courriel { get; set; }
+        public string CarteDeCredit { get; set; }
+        public int NumeroDeChambre { get; set; }
+
+        public Client(string nom, string prenom, string courriel, string carteDeCredit, int numeroDeChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteDeCredit = carteDeCredit;
+            NumeroDeChambre = numeroDeChambre;
+        }
+    }
+}
